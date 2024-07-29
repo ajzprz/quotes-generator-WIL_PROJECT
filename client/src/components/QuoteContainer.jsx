61dc1acbbd2faa996d71a3c489236a6a8ca778da
Ajaya Prajapati <ajz.prz@gmail.com>
2024-07-29 12:59:48 -0400

    share quote on twitter completed

--- client/src/components/QuoteContainer.jsx
+++ client/src/components/QuoteContainer.jsx
@@ -4,7 +4,9 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function QuoteContainer() {
-  const [quote, setQuote] = useState([{quote : "I am gay", author : "Gopal Gautam"}]);
+  const [quote, setQuote] = useState([
+    { quote: "Click on Refresh Button ", author: "Author Name" },
+  ]);
   async function generateQuotes() {
     const category = "happiness";
     const apiKey = "/8dg23S4dvR8beZ04xbfPw==MwHtyTtAnkSbrC95";
@@ -30,37 +32,44 @@ function QuoteContainer() {
     // generateQuotes();
   }, []);
 
-  return (
-    <div className="self-center m-auto  bg-sky-400 bg-opacity-10 rounded-md h-[50vh] w-[60vw] flex flex-col justify-between items-center">
-      <div className="flex h-80 w-full p-4 quote-text text-white text-center text-2xl justify-center items-center">
-        <span>{quote[0].quote}</span>
-      </div>
+  const twiterPost = () => {
+    window.open(
+      `https://x.com/intent/post?text="${quote[0].quote}" - ${quote[0].author}`
+    );
+  };
 
-      <div className="w-[80%] m-auto flex justify-between p-4 text-white">
-        <div className="flex justify-center">
-          <h1 className="font-medium text-lg px-5 py-2.5 mb-2">
-            {quote[0].author}
-          </h1>
-        </div>
-        <div className="flex gap-7">
-          <button
-            onClick={generateQuotes}
-            type="button"
-            className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 transition-colors"
-            aria-label="Generate new quote"
-          >
-            <TbRefresh />
-          </button>
-          <button
-            type="button"
-            className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 transition-colors"
-            aria-label="Share on Twitter"
-          >
-            <FaXTwitter />
-          </button>
-        </div>
+  return (
+    <div className="self-center m-auto shadow-lg bg-sky-400 bg-opacity-10 rounded-md h-[50vh] w-[90vw] md:w-[70vw] lg:w-[60vw] flex flex-col justify-between items-center p-4">
+    <div className="flex h-60 md:h-80 w-full p-4 quote-text text-white text-center text-xl md:text-2xl justify-center items-center">
+      <span>{quote[0].quote}</span>
+    </div>
+
+    <div className="w-full md:w-[80%] m-auto flex flex-col md:flex-row justify-between items-center p-4 text-white gap-4">
+      <div className="flex justify-center">
+        <h1 className="font-medium text-lg px-5 py-2.5 mb-2 text-center">
+          {quote[0].author}
+        </h1>
+      </div>
+      <div className="flex gap-4 md:gap-7">
+        <button
+          onClick={generateQuotes}
+          type="button"
+          className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 transition-colors"
+          aria-label="Generate new quote"
+        >
+          <TbRefresh />
+        </button>
+        <button
+        onClick={twiterPost}
+          type="button"
+          className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-lg px-5 py-2.5 mb-2 transition-colors"
+          aria-label="Share on Twitter"
+        >
+          <FaXTwitter />
+        </button>
       </div>
     </div>
+  </div>
   );
 }
 
